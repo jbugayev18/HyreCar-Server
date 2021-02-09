c91131e1c04662f5f8d1efb1bdc03a61eb069b15
Julia Bugayev <jbugayev18@gmail.com>
2021-02-09 13:53:29 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const morgan = require("morgan");
+
+//Creates new application object that encapsulates the functionality of Express server.
+const app = express();
+//This is middleware that requests pass through
+//on their way to the final handler
+app.use(morgan("dev"));
+
+//This is the final request handler
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.get("/echo", (req, res) => {
+  const responseText = `Here are some details of your request:
+  ID: ${req.ID}
+  Make: ${req.make}
+  Model: ${req.model}
+  Year: ${req.year}
+  VIN" ${req.VIN}
+  `;
+
+  res.send(responseText);
+});
+
+app.get("/queryViewer", (req, res) => {
+  console.log(req.query);
+  res.end(); // does not send any data to the client
+});
+
+app.get("/cars"),
+  (req, res) => {
+    //1. get values from the request
+    const id = req.query.id;
+    const make = req.query.make;
+    const model = req.query.model;
+    const year = req.query.year;
+    const vin = req.query.vin;
+    //2. validate the values
+    if (!id) {
+      //3. id was not provided
+      return res.status(400).send("Please provide an ID");
+    }
+
+    if (!make) {
+      //3. make was not provided
+      return res.status(400).send("Please provide a make");
+    }
+
+    if (!model) {
+      //3. model was not provided
+      return res.status(400).send("Please provide a model");
+    }
+
+    if (!year) {
+      //3. year was not provided
+      return res.status(400).send("Please provide a year");
+    }
+
+    if (!vin) {
+      //3. vin was not provided
+      return res.status(400).send("Please provide a vin");
+    }
+
+    //4. If all values are valid
+    const registeredCar = `Your car: ${id}, ${make}, ${model}, ${year}, and ${vin} is successfully registered!`;
+
+    //5. send the response
+    res.send(registeredCar);
+  };
+
+app.get("/greetings", (req, res) => {
+  //1. get values from the request
+  const name = req.query.name;
+  const race = req.query.race;
+
+  //2. validate the values
+  if (!name) {
+    //3. name was not provided
+    return res.status(400).send("Please provide a name");
+  }
+
+  if (!race) {
+    //3. race was not provided
+    return res.status(400).send("Please provide a race");
+  }
+
+  //4. and 5. both name and race are valid so do the processing.
+  const greeting = `Greetings ${name} the ${race}, welcome to our kingdom.`;
+
+  //6. send the response
+  res.send(greeting);
+});
+
+app.listen(8000, () => {
+  console.log("Express server is listening on port 8000!");
+});
