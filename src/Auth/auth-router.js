5299dd60f0ca18e2b0a3c2595bc00c91a7b94c97
Julia Bugayev <jbugayev18@gmail.com>
2021-02-18 13:18:39 -0800

    Updating server side

--- src/Auth/auth-router.js
+++ src/Auth/auth-router.js
@@ -41,27 +41,4 @@ authRouter.post("/signin", jsonBodyParser, (req, res, next) => {
     .catch(next);
 });
 
-// app.get("/greetings", (req, res) => {
-//   //1. get values from the request
-//   const name = req.query.name;
-//   const race = req.query.race;
-
-//   //2. validate the values
-//   if (!name) {
-//     //3. name was not provided
-//     return res.status(400).send("Please provide a name");
-//   }
-
-//   if (!race) {
-//     //3. race was not provided
-//     return res.status(400).send("Please provide a race");
-//   }
-
-//   //4. and 5. both name and race are valid so do the processing.
-//   const greeting = `Greetings ${name} the ${race}, welcome to our kingdom.`;
-
-//   //6. send the response
-//   res.send(greeting);
-// });
-
 module.exports = authRouter;
